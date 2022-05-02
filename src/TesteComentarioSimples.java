a7a552c04153077fa5e8e70d4e89beed73059c67
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 20:05:37 -0300

    Teste de uso do comentário de uma linha em Java.

---
+++ src/TesteComentarioSimples.java
@@ -0,0 +1,7 @@
+public class TesteComentarioSimples {
+
+    public static void main(String[] args) {
+        // Realiza o cálculo e exibe o resultado em tela
+        System.out.println(25 + 80 + 10);
+    }
+}
